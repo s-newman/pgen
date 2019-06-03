0070db829cec21aa5d1bc2afaf943aba060f5c57
Sean Newman <sxn6296@rit.edu>
2019-06-02 22:26:31 -0400

    Print password of numbers, symbols, and words

--- src/pgen.cpp
+++ src/pgen.cpp
@@ -11,17 +11,24 @@
 
 int main(void)
 {
-    enum Wordlist param = FULL;
+    enum Wordlist param = SHORT;
     std::vector<std::string> wordlist = get_wordlist(param);
+    param = NUMBERS;
+    std::vector<std::string> numblist = get_wordlist(param);
+    param = SYMBOLS;
+    std::vector<std::string> symblist = get_wordlist(param);
+    std::vector<std::string> charlist = numblist;
+    charlist.insert(charlist.end(), symblist.begin(), symblist.end());
 
     if(!random_init())
     {
         return EXIT_FAILURE;
     }
 
-    for(int idx = 0; idx < 4; idx++)
+    for(int idx = 0; idx < 3; idx++)
     {
         std::cout << wordlist[random_int(wordlist.size())];
+        std::cout << charlist[random_int(charlist.size())];
     }
     std::cout << std::endl;
 
