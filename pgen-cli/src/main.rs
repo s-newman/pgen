6eb7ab5661d593f1b51e3a91758fd83b4a9d3972
Sean Newman <sean@snewman.co>
2020-12-16 15:37:58 -0500

    Configure a ton of lints

--- pgen-cli/src/main.rs
+++ pgen-cli/src/main.rs
@@ -1,4 +1,36 @@
-#![warn(clippy::all, clippy::pedantic)]
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    clippy::cargo,
+
+    // Restriction lints
+    clippy::as_conversions,
+    clippy::dbg_macro,
+    clippy::decimal_literal_representation,
+    clippy::else_if_without_else,
+    clippy::exit,
+    clippy::expect_used,
+    clippy::filetype_is_file,
+    clippy::float_cmp_const,
+    clippy::get_unwrap,
+    clippy::lossy_float_literal,
+    clippy::mem_forget,
+    clippy::missing_docs_in_private_items,
+    clippy::multiple_inherent_impl,
+    clippy::panic,
+    clippy::panic_in_result_fn,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::todo,
+    clippy::unimplemented,
+    clippy::unneeded_field_pattern,
+    clippy::unreachable,
+    clippy::unwrap_in_result,
+    clippy::unwrap_used,
+    clippy::use_debug,
+    clippy::verbose_file_reads,
+    clippy::wrong_pub_self_convention,
+)]
+
 use structopt::StructOpt;
 
 use pgen_core::pattern::Pattern;
